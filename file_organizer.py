4b090bd4c76fa2a611a2ce10c1ed3708ecb817df
ErPyCode <erpycode@gmail.com>
2026-05-15 00:43:09 +0330

    complet menu and add Dry mode

--- file_organizer.py
+++ file_organizer.py
@@ -37,14 +37,29 @@ def move_to_directories():
                 print(f"✅ {files.name} copy to {cat} ")
 
 
+def dry_mode():
+    for files in base_path.glob("*"):
+        for cat, extension in FILE_CATEGORIES.items():
+            if files.suffix in extension:
+                print(f"✅ {files.name} will be copy / move to {cat} ")
+
+
 print("*" * 20, "\n")
 print("📁 File Organizer \n")
 print("*" * 20, "\n")
 
-print("1.🔴 Move File To Folders \n ")
-print("2.🔴 Copy Files To Folfers \n")
+print("1.🟡 Move File To Folders \n ")
+print("2.🔵 Copy Files To Folfers \n")
+print("3.🟢 Dry Mode \n")
+print("4.🔴 Exit \n")
 
-user_choice=input("❓: ")
+user_choice = input("❓: ")
 
-if user_choice=="1":
-    
+if user_choice == "1":
+    move_to_directories()
+elif user_choice == "2":
+    copy_to_directories()
+elif user_choice == "3":
+    dry_mode()
+elif user_choice == "4":
+    exit()
